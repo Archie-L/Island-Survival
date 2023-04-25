818679ecdc1a8f3171eef4a1eeadc48356237634
Archie-L <526737@chrome.escg.ac.uk>
2023-04-25 15:11:44 +0100

    .

--- Assets/tool.cs
+++ Assets/tool.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class tool : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Animator anim;
+    public List<string> type;
+    public bool isAxe;
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            anim.SetTrigger("swing");
+            Invoke("ShootRay", 0.2f);
+        }
+    }
+
+    void ShootRay()
+    {
+        for (int i = 0; i < 1; i++)
+        {
+            Transform camTransform = Camera.main.transform;
+            RaycastHit hit;
+
+            if (Physics.Raycast(camTransform.position, camTransform.forward, out hit, 2))
+            {
+                if (hit.collider.tag == ("stone"))
+                {
+                    Debug.Log("poopoo");
+                }
+                if (hit.collider.tag == ("wood"))
+                {
+                    Debug.Log("peepee");
+                }
+            }
+        }
     }
 }
